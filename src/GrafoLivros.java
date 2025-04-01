4c4f8e553cdb8bd562539aff254d4e971fb9b14f
llbr <williavi2024@gmail.com>
2025-03-31 23:43:17 -0300

    att commit

--- src/GrafoLivros.java
+++ src/GrafoLivros.java
@@ -1,2 +1,18 @@
+import java.util.*;
+
 public class GrafoLivros {
+   private Map<Livro, Set<Livro>> grafo;
+
+   public GrafoLivros(){
+       this.grafo = new HashMap<>();
+   }
+
+   public void adicionarLivro(Livro livro) {
+       grafo.putIfAbsent(livro, new HashSet<>());
+   }
+
+    public void adicionarRelacao(Livro livro1, Livro livro2) {
+        grafo.get(livro1).add(livro2);
+        grafo.get(livro2).add(livro1);
+    }
 }
--- src/Livro.java
+++ src/Livro.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.Objects;
 
 public class Livro {
     private String titulo;

e20dd52132cbc95b82e4313a571c740e88fd9a07
llbr <williavi2024@gmail.com>
2025-03-31 21:17:20 -0300

    first commit

---
+++ src/Livro.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Livro {
+    private String titulo;
+    private String autor;
+
+
+    //Construtor
+    public Livro(String titulo, String autor) {
+        this.titulo = titulo;
+        this.titulo = titulo;
+    }
+
+    //getter
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    //@Override é uma anotação (annotation) usada para indicar que um métod está sendo sobrescrito (overridden) de uma superclasse ou de uma interface que a classe está implementando
+    @Override
+    public boolean equals
+}
